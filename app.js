793f98848ce255444b156b46b37c1526af3c349a
dhanushvarman <dhanushvarmanj@gmail.com>
2022-12-04 12:08:20 +0530

    old is gold

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var employeeRouter = require('./routes/employee');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
